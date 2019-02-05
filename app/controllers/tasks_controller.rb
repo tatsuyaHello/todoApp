920a02d1e0b60fe82b11fa24e9622a3c3fb81af8
tatsuya <ryuuma1005@gmail.com>
2019-02-05 22:20:32 +0900

    finish delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -36,6 +36,13 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def destroy 
+		@task = Task.find(params[:id])
+		@task.destroy 
+		flash[:notice] = "Destroy a task"
+		redirect_to tasks_path 
+	end
+
 	private
 		def task_params
 			params.require(:task).permit(:content, :deadline, :done, :comment)
