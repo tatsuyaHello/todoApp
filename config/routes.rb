417dd773f67a8a2d5af872a77b612a3ff8dc0f75
tatsuya <ryuuma1005@gmail.com>
2019-02-06 20:51:27 +0900

    homepage finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'homepage/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tasks#index'
   resources :tasks
+
+  get 'signup' => 'users/new'
 
 end
