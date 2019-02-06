c169d96c851ecc1b9ac5e31f6683be786cc9584a
tatsuya <ryuuma1005@gmail.com>
2019-02-06 21:27:03 +0900

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_095654) do
+ActiveRecord::Schema.define(version: 2019_02_06_121619) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.date "deadline"
     t.boolean "done"
     t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
